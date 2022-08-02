c549e1485e717376eab018ac9df6934d91352c34
jlewisit <joelewis.mortgage@gmail.com>
2022-08-02 06:37:18 -0700

    final rough

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -4,10 +4,6 @@ var cityName = document.querySelector('.cityName');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
 var input = document.querySelector('.input');
-// var city  = document.querySelector('.city_name');
-// var temperature = document.querySelector('.temperature');
-// var humidity = document.querySelector('.humidity');
-// var weather = document.querySelector('.weather'); 
 
     button.addEventListener('click', getWeather())
 
@@ -28,17 +24,9 @@ var input = document.querySelector('.input');
   };
                 
 function WriteHTML() {
-  // var tempValue = data['main']['temp'];
-  // var cityValue = data['name'];
-  // var weatherValue = data['weather'][0]['description'];
-  // var humidityValue = data['main']['humidity'];
-
   name.innerHTML = nameValue;
   temp.innerHTML = tempValue;
   desc.innerHTML = descValue;
-  // city.innerHTML = "weather at "+ cityValue;
-  // weather.innerHTML = "weather: "+ weatherValue;
-  // temperature.innerHTML = "temperature: "+ tempValue + " celcius";
-  // humidity.innerHTML = "humidity: "+ humidityValue;
+
 }
 
