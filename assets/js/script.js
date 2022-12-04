b88f9f7b092e9f7a1084190eafcdbf56cd735f13
jlewisit <joelewis.mortgage@gmail.com>
2022-12-04 12:43:12 -0800

    code cleanup

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,77 +14,7 @@ var currentUvEl = document.querySelector("#uv-input")
 
 var cityArray = [];
 
-// // Function to display search history list
-// function renderSearchHistory() {
-//   searchHistoryContainer.innerHTML = "";
-//   //Start at end of history array and count down, showing the most recent at top
-//   for (var i = searchHistory.length - 1; i >= 0; i--) {
-//     var btn = document.createElement("button");
-//     btn.setAttribute("type", "button");
-//     btn.setAttribute("aria-controls", "today forecast");
-//     btn.classList.add("history-btn", "btn-history");
-//   }
-// }
-
-// Getting weather from the One Call API using coordinates
-function getWeather(lat, lon) {
-  console.log(lat, lon, "getWeather running");
-  var apiUrl2 =
-    "https://api.openweathermap.org/data/3.0/onecall?lat=" +
-    lat +
-    "&lon=" +
-    lon +
-    "&exclude=hourly,daily&appid=" +
-    apiKey;
-  fetch(apiUrl2)
-    .then(function (response) {
-      if (response.ok) {
-        console.log(response);
-        response
-          .json()
-          .then(function (data) {
-            console.log(data);
-          })
-          .catch(function (err) {
-            console.log(err);
-          });
-      }
-    })
-    .then((data) => {
-      // var cityNameValue = data[0].name;
-      var tempValue = data["main"]["temp"];
-      var descValue = data["weather"][0]["description"];
-
-      // cityName.innerHTML = cityNameValue;
-      temp.innerHTML = tempValue;
-      desc.innerHTML = descValue;
-    });
-}
-
-// Get coordinates for the city/state from the Geocoding API
-// Calls get Weather and extracts latitude and longitude from the array
-function getLatLon(event) {
-  event.preventDefault();
-  var apiUrl =
-    "http://api.openweathermap.org/geo/1.0/direct?q=" +
-    inputValue.value +
-    "&limit=5&appid=" +
-    apiKey;
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      console.log(response);
-      response.json().then(function (data) {
-        console.log(data);
-        console.log("getLatLon ran");
-        getWeather(data[0].lat, data[0].lon);
-      });
-    } else {
-      alert("Error: " + response.statusText);
-    }
-  });
-}
-
-// search city form submission 
+// City search form submission 
 var formSubmitHandler = function(event) {
   event.preventDefault();
 
